ec07baf9aa4bd4c62dccae3d09f605e6e3aff7e2
HAnnahc77 <HAnnahc77@nycstudents.net>
2025-02-14 14:09:15 -0500

    cccsc

--- vue-reactivity-project/src/components/CartCard.vue
+++ vue-reactivity-project/src/components/CartCard.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="cartCard">
     <h2>{{ album.name }}</h2>
-    <h3>{{ album.band }}</h3>
     <h2>{{ album.price }}</h2>
   </div>
 </template>
@@ -30,21 +29,10 @@ defineProps({
   margin: 10px 0;
 }
 
-.cartCard h3 {
-  font-size: 1.2em;
-  color: #777;
-  margin: 5px 0;
-}
-
 .cartCard h2:last-of-type {
   font-size: 1.4em;
   font-weight: 600;
   color: #e74c3c;
   margin-top: 10px;
-}
-
-.cartCard h2,
-.cartCard h3 {
-  margin: 0;
 }
 </style>
--- vue-reactivity-project/src/views/CartView.vue
+++ vue-reactivity-project/src/views/CartView.vue
@@ -2,6 +2,7 @@
   <main>
     <div class="cartCards">
       <CartCard v-for="album in AlbumInCartArray" :key="album.name" :album="album" />
+      <h2 v-if="AlbumInCartArray.length > 0">Total:{{ calcTotal(AlbumInCartArray) }}</h2>
     </div>
   </main>
 </template>
@@ -9,6 +10,13 @@
 <script setup>
 import { AlbumInCartArray } from '../assets/AlbumArray'
 import CartCard from '../components/CartCard.vue'
+
+function calcTotal(AlbumInCartArray) {
+  let total = 0
+  AlbumInCartArray.forEach((album) => {
+    total += album.price
+  })
+}
 </script>
 
 <style scoped>
